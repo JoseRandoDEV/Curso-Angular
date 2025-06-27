e67fa73c18737c3c9f8e3bdd507f2293d89edf74
JoseRandoDEV <joseandresrando@gmail.com>
2025-06-26 21:14:12 -0300

    Clase 11 ngFor

---
+++ ngfor/src/app/app.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  listadoDeEstudiantes: any = [] =[
+    { nombre: 'Juan', estado: 'Promocionado' },
+    { nombre: 'Ana', estado: 'Regular' },
+    { nombre: 'Luis', estado: 'Regular' },
+    { nombre: 'María', estado: 'Recursante' },
+  ]
+}
