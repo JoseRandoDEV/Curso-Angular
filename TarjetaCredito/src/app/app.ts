06bf146a6d905290f47ab163eba044e10b7047cf
JoseRandoDEV <joseandresrando@gmail.com>
2025-07-12 21:42:58 -0300

    Clase 22 Finalizada

--- TarjetaCredito/src/app/app.ts
+++ TarjetaCredito/src/app/app.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CrearTarjeta } from "./components/crear-tarjeta/crear-tarjeta";
+import { ListarTarjeta } from "./components/listar-tarjeta/listar-tarjeta";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CrearTarjeta, ListarTarjeta],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
