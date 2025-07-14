e3100487fc0eb1c8aadf81a62fd67d1e86f50225
JoseRandoDEV <joseandresrando@gmail.com>
2025-07-14 12:42:34 -0300

    Clase 23 Finalizada

--- TarjetaCredito/src/app/components/crear-tarjeta/crear-tarjeta.ts
+++ TarjetaCredito/src/app/components/crear-tarjeta/crear-tarjeta.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-tarjeta',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './crear-tarjeta.html',
   styleUrl: './crear-tarjeta.css'
 })
-export class CrearTarjeta {
+export class CrearTarjeta implements OnInit {
 
+  forms: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.forms = this.fb.group({
+      titular: ['', Validators.required],
+      nroTarjeta: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+      fechaExpiracion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5) ]],
+      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3) ]],
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  crearTarjeta(){
+    console.log(this.forms)
+  }
 }
