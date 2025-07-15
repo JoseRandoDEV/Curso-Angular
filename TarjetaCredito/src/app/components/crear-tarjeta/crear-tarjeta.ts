36d484027baf7d0be0ca14875a6611fb03ee08b3
JoseRandoDEV <joseandresrando@gmail.com>
2025-07-15 18:01:41 -0300

    Clase 24 Finalizada  Configuracion Firebase

--- TarjetaCredito/src/app/components/crear-tarjeta/crear-tarjeta.ts
+++ TarjetaCredito/src/app/components/crear-tarjeta/crear-tarjeta.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TarjetaCredito } from '../../models/tarjeta';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { initializeApp } from '@angular/fire/app';
+import { getAnalytics } from '@angular/fire/analytics';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-crear-tarjeta',
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    AngularFireModule,
+    AngularFirestoreModule
+  ],
   templateUrl: './crear-tarjeta.html',
   styleUrl: './crear-tarjeta.css'
 })
@@ -16,15 +26,28 @@ export class CrearTarjeta implements OnInit {
     this.forms = this.fb.group({
       titular: ['', Validators.required],
       nroTarjeta: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
-      fechaExpiracion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5) ]],
-      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3) ]],
+      fechaExpiracion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
     })
   }
 
   ngOnInit(): void {
   }
 
-  crearTarjeta(){
+  crearTarjeta() {
     console.log(this.forms)
+
+    const TARJETA: TarjetaCredito = {
+      titular: this.forms.value.titular,
+      numDeTarjeta: this.forms.value.numDeTarjeta,
+      cvv: this.forms.value.cvv,
+      fechaActualizacion: new Date(),
+      fechaCreacion: new Date(),
+      fechaExpiracion: this.forms.value.fechaExpiracion
+    }
+    console.log(TARJETA)
   }
+
 }
+const app = initializeApp(environment.firebaseConfig);
+const analytics = getAnalytics(app);
